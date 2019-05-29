bcb41a87769885cc58d46f79e02cd6c1d16ff4d2
Pavel <davidov.pavel@gmail.com>
2019-05-29 17:45:30 +0300

    creating entry

---
+++ routes/entries.js
@@ -0,0 +1,19 @@
+
+exports.form = (req, res) => res.render('post', {
+    title: 'Post'
+});
+
+exports.submit = (req, res, next) => {
+    const data = req.body.entry;
+    const user = res.locals.user;
+    const username = user ? user.name : null;
+    const entry = new Entry({
+        username,
+        title: data.title,
+        body: data.body
+    });
+    entry.save((err) => {
+        if (err) return next(err);
+        res.redirect('/');
+    });
+};
